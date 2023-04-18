967023f48a2ce52ca0ca5dde185990abd201f196
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2023-04-17 23:47:43 -0300

    Adicionando as imagens do jogo

--- FlappyBird.py
+++ FlappyBird.py
@@ -2,9 +2,29 @@ import pygame as game
 import os
 import random
 
+import pygame.image
 
-TELA_LARGURA = 500
-TELA_ALTURA  = 800
+#Frame de exibição
+telaLargura = 500
+telaAltura  = 800
+
+#Importando as imagens
+imageCano  = pygame.transform.scale2x(pygame.image.load("/Users/PedroVitorPereira/Documents"
+                         "/GitHub/Python_Projects/Flappy-Bird/imagens/pipe.png"))
+
+imagemChao = pygame.transform.scale2x(pygame.image.load("/Users/PedroVitorPereira/Documents"
+                        "/GitHub/Python_Projects/Flappy-Bird/imagens/base.png"))
+imagemFundo  = pygame.transform.scale2x(pygame.image.load("/Users/PedroVitorPereira/Documents"
+                            "/GitHub/Python_Projects/Flappy-Bird/imagens/bg.png"))
+
+
+##As imagems do passaroa para dar a impressão de estar voando será
+# necessário utilizar uma lista que pegue esses vetores.
+imagemPassaro = pygame.transform.scale2x(pygame.image.load("/Users/PedroVitorPereira/"
+        "Documents/GitHub/Python_Projects/Flappy-Bird/imagens/bird1.png"))
+
+
+
 
 
 
